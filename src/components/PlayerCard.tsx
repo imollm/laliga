99298fa68723cb59e396c9a85dba67a2956df1e6
ivan <ivanmoll07@gmail.com>
2024-02-18 20:33:36 +0100

    ✏️ refactor: Change player card styles

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -17,35 +17,35 @@ const PlayerCard = ({ player }: { player: IPlayersData }) => {
                 <figure className="m-0 p-0 w-20">
                     <UserIcon />
                 </figure>
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white pt-3">
+                <h5 className="mb-1 text-xl font-mediu text-white pt-3">
                     {player.name}
                 </h5>
-                <div className="flex flex-col gap-3 text-lg text-gray-500 dark:pt-4">
-                    <div className="text-gray-900 dark:text-white">
+                <div className="flex flex-col gap-3 text-lg text-gray-500 pt-4">
+                    <div className="text-white">
                         <span className="font-bold">Posición </span>
-                        <span className='dark:text-gray-400'>{player.position[0].toUpperCase() + player.position.slice(1, player.position.length)}</span>
+                        <span className='text-gray-400'>{player.position[0].toUpperCase() + player.position.slice(1, player.position.length)}</span>
                     </div>
                     <div className="flex flex-row gap-5 justify-between">
-                        <div className="dark:text-white">
-                            <span className="font-bold">PJ <span className="dark:text-gray-400">{player.matchesPlayed}</span></span>
+                        <div className="text-white">
+                            <span className="font-bold">PJ <span className="text-gray-400">{player.matchesPlayed}</span></span>
                         </div>
-                        <div className="dark:text-white">
-                            <span className="font-bold">PG <span className="dark:text-gray-400">{player.matchesWon}</span></span>
+                        <div className="text-white">
+                            <span className="font-bold">PG <span className="text-gray-400">{player.matchesWon}</span></span>
                         </div>
-                        <div className="dark:text-white">
-                            <span className="font-bold">PP <span className="dark:text-gray-400">{player.matchesLost}</span></span>
+                        <div className="text-white">
+                            <span className="font-bold">PP <span className="text-gray-400">{player.matchesLost}</span></span>
                         </div>
                     </div>
                     <div className="flex flex-row gap-5 justify-start">
-                        <div className="dark:text-white">
-                            <span className="font-bold">SG <span className="dark:text-gray-400">{player.setsWon}</span></span>
+                        <div className="text-white">
+                            <span className="font-bold">SG <span className="text-gray-400">{player.setsWon}</span></span>
                         </div>
-                        <div className="dark:text-white">
-                            <span className="font-bold">SP <span className="dark:text-gray-400">{player.setsLost}</span></span>
+                        <div className="text-white">
+                            <span className="font-bold">SP <span className="text-gray-400">{player.setsLost}</span></span>
                         </div>
                     </div>
-                    <div className="dark:text-white">
-                        <span className="font-bold">JG <span className='dark:text-gray-400'>{player.gamesWon}</span></span>
+                    <div className="text-white">
+                        <span className="font-bold">JG <span className='text-gray-400'>{player.gamesWon}</span></span>
                     </div>
                 </div>
             </div>
